11810f6759e374156ede0f2948fccf49f88730aa
Hélder Gonçalves <helderpimentaxc@gmail.com>
2021-05-17 22:46:24 +0100

    Camera and QRCode Reader Implementation

--- screens/Camera.js
+++ screens/Camera.js
@@ -1,13 +1,34 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { Component } from "react";
 
+import { Linking, View, StyleSheet} from "react-native";
+
+import QRCodeScanner from "react-native-qrcode-scanner";
+import { RNCamera } from "react-native-camera";
+
+onSuccess = (e) => {
+  Linking.openURL(e.data).catch((err) =>
+    console.error("An error occured", err)
+  );
+};
 const Camera = ({ navigation }) => {
   return (
-    <View>
-      <Text style={{ paddingTop: 200, textAlign: "center" }}>Camera</Text>
+    <View style={styles.container}>
+      <QRCodeScanner
+        reactivate={true}
+        onRead={this.onSuccess}
+        flashMode={RNCamera.Constants.FlashMode.torch}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  }
+})
 
 export default Camera;
 // Dummy Datas

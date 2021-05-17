bbc369a9c79058c94e68ee04540f6cbeb71d3748
Hélder Gonçalves <helderpimentaxc@gmail.com>
2021-05-17 21:56:29 +0100

    Bottom_Nav_Bar Camera Added Icons

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -100,118 +100,117 @@ const CustomTabBar = (props) => {
 
 const Tabs = () => {
     return (
-        <Tab.Navigator
-            tabBarOptions={{
-                showLabel: false,
-                style: {
-                    position: 'absolute',
-                    left: 0,
-                    bottom: 0,
-                    right: 0,
-                    borderTopWidth: 0,
-                    backgroundColor: "transparent",
-                    elevation: 0
-                }
-            }}
-            tabBar={(props) => (
-                <CustomTabBar
-                    props={props}
-                />
-            )}
-        >
-            <Tab.Screen
-                name="Home"
-                component={Home}
-                options={{
-                    tabBarIcon: ({ focused }) => (
-                        <Image
-                            source={icons.cutlery}
-                            resizeMode="contain"
-                            style={{
-                                width: 25,
-                                height: 25,
-                                tintColor: focused ? COLORS.primary : COLORS.secondary
-                            }}
-                        />
-                    ),
-                    tabBarButton: (props) => (
-                        <TabBarCustomButton
-                            {...props}
-                        />
-                    )
+      <Tab.Navigator
+        tabBarOptions={{
+          showLabel: false,
+          style: {
+            position: "absolute",
+            left: 0,
+            bottom: 0,
+            right: 0,
+            borderTopWidth: 0,
+            backgroundColor: "transparent",
+            elevation: 0,
+          },
+        }}
+        tabBar={(props) => <CustomTabBar props={props} />}
+      >
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Image
+                source={icons.cutlery}
+                resizeMode="contain"
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? COLORS.primary : COLORS.secondary,
                 }}
-            />
+              />
+            ),
+            tabBarButton: (props) => <TabBarCustomButton {...props} />,
+          }}
+        />
 
-            <Tab.Screen
-                name="Search"
-                component={Home}
-                options={{
-                    tabBarIcon: ({ focused }) => (
-                        <Image
-                            source={icons.search}
-                            resizeMode="contain"
-                            style={{
-                                width: 25,
-                                height: 25,
-                                tintColor: focused ? COLORS.primary : COLORS.secondary
-                            }}
-                        />
-                    ),
-                    tabBarButton: (props) => (
-                        <TabBarCustomButton
-                            {...props}
-                        />
-                    )
+        <Tab.Screen
+          name="Search"
+          component={Home}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Image
+                source={icons.search}
+                resizeMode="contain"
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? COLORS.primary : COLORS.secondary,
                 }}
-            />
+              />
+            ),
+            tabBarButton: (props) => <TabBarCustomButton {...props} />,
+          }}
+        />
 
-            <Tab.Screen
-                name="Like"
-                component={Home}
-                options={{
-                    tabBarIcon: ({ focused }) => (
-                        <Image
-                            source={icons.like}
-                            resizeMode="contain"
-                            style={{
-                                width: 25,
-                                height: 25,
-                                tintColor: focused ? COLORS.primary : COLORS.secondary
-                            }}
-                        />
-                    ),
-                    tabBarButton: (props) => (
-                        <TabBarCustomButton
-                            {...props}
-                        />
-                    )
+        <Tab.Screen
+          name="Camera"
+          component={Home}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Image
+                source={icons.camera}
+                resizeMode="contain"
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? COLORS.primary : COLORS.secondary,
                 }}
-            />
+              />
+            ),
+            tabBarButton: (props) => <TabBarCustomButton {...props} />,
+          }}
+        />
 
-            <Tab.Screen
-                name="User"
-                component={Home}
-                options={{
-                    tabBarIcon: ({ focused }) => (
-                        <Image
-                            source={icons.user}
-                            resizeMode="contain"
-                            style={{
-                                width: 25,
-                                height: 25,
-                                tintColor: focused ? COLORS.primary : COLORS.secondary
-                            }}
-                        />
-                    ),
-                    tabBarButton: (props) => (
-                        <TabBarCustomButton
-                            {...props}
-                        />
-                    )
+        <Tab.Screen
+          name="Like"
+          component={Home}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Image
+                source={icons.like}
+                resizeMode="contain"
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? COLORS.primary : COLORS.secondary,
                 }}
-            />
-        </Tab.Navigator>
-    )
+              />
+            ),
+            tabBarButton: (props) => <TabBarCustomButton {...props} />,
+          }}
+        />
+
+        <Tab.Screen
+          name="User"
+          component={Home}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Image
+                source={icons.user}
+                resizeMode="contain"
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? COLORS.primary : COLORS.secondary,
+                }}
+              />
+            ),
+            tabBarButton: (props) => <TabBarCustomButton {...props} />,
+          }}
+        />
+      </Tab.Navigator>
+    );
 }
 
 export default Tabs

05a12949babe6415b4a60f9131e7ab6dac97f17a
Pedro Sousa <pedrosousa1@sapo.pt>
2021-06-03 17:42:09 +0100

    Leitura de mesa + AsyncStorage
    Leitura do QR Code composto pelo id da mesa + Guardar id lido via AsyncStorage

--- screens/Camera.js
+++ screens/Camera.js
@@ -1,33 +1,47 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 
-import { Linking, View, StyleSheet} from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 
 import QRCodeScanner from "react-native-qrcode-scanner";
 import { RNCamera } from "react-native-camera";
 
-onSuccess = (e) => {
-  Linking.openURL(e.data).catch((err) =>
-    console.error("An error occured", err)
-  );
-};
-const Camera = ({ navigation }) => {
+import AsyncStorage from "@react-native-community/async-storage";
+
+function Camera({ navigation }) {
+  var [ idMesa, setIdMesa ] = useState("");
+
+  getMesa = () => {
+    fetch("https://one-fork.herokuapp.com/api/Organization")
+      .then((response) => response.json())
+      .then((data) => {
+        setRestaurantes(data);
+        checkIfImInRestaurant(data);
+      });
+  };
+
+  onSuccess = async (e) => {
+    setIdMesa(e.data);
+    await AsyncStorage.setItem("idMesa", e.data);
+    /*Linking.openURL(e.data).catch((err) =>
+      console.error("An error occured", err)
+    );*/
+  };
+
   return (
     <View style={styles.container}>
-      <QRCodeScanner
-        reactivate={true}
-        onRead={this.onSuccess}
-      />
+      <QRCodeScanner reactivate={true} onRead={onSuccess} />
+      <Text>{idMesa}</Text>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-  }
-})
+  },
+});
 
 export default Camera;
 // Dummy Datas
